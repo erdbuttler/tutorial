f679efd4f96e3832945ce6c6b3b8a862552e18ad
Nikolaus Kelis <id101507@fhstp.ac.at>
2014-04-01 09:58:49 +0200

    2nd commit - with some simple code to check the changes.

--- egit_tutorial/src/core/Tutorial.java
+++ egit_tutorial/src/core/Tutorial.java
@@ -4,6 +4,11 @@ public class Tutorial {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a = 4;
+		int b = 5;
+		if((a + b) == (b + a)) {
+			System.out.print("true");
+		}
 
 	}
 
